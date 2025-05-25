68fdc880cca14c704d4f09d476e7f03b0e2929c3
Aarushi <aarushimandloi744@gmail.com>
2025-05-25 22:56:34 +0530

    4-lec

---
+++ 01_basics/variables.js
@@ -0,0 +1,26 @@
+var accountPassword = "1234"
+console.log(accountPassword);
+
+var accountPassword =  "uh8834"
+console.log(accountPassword);
+
+var accountPassword = "uhufwru";
+console.log(accountPassword);
+
+accountPassword = "withoutVar";
+console.log(accountPassword);
+// var has functional scope and hositing and it can redecalerd as you can see abhove exapmles  
+
+let accountEmail = "aarushimandloi744@gmail.com"
+console.log(accountEmail);
+
+accountEmail = "ehfef";
+console.log(accountEmail);
+//let has no shadowing no hoisting AND we can change the value BLOCK SCOPE
+
+const account_Id = 146251
+
+console.log(account_Id);
+// const we can only initialize it. and there is BLOCK SCOPE  AND no shadowing no hoisting
+
+// node 01_basics/variables.js 
