280062f86a1b68623e889d5e32748d52f0eaf0ca
Aarushi <aarushimandloi744@gmail.com>
2025-05-28 07:48:29 +0530

    strings

--- 01_basics/tempCodeRunnerFile.js
+++ 01_basics/tempCodeRunnerFile.js
@@ -1,3 +1,31 @@
-function MyFunction(){
-//     console.log("Learning javascript with chai aur javascript ")
-// }
+const { use } = require("react");
+
+// const strDeclare = new String('A@ru.shi'); // this is also declaration of str
+// console.log(strDeclare);  // [String: 'A@ru.shi']
+
+// const username = " Shubhman";    // declare of str
+// const age = 24
+
+// console.log(username + age + " Cricketer"); // outdated don't use it.
+
+// console.log(`Hello I am ${username} , I'm ${age} year old and I'm crickter. who is ${strDeclare}`);
+
+// console.log(username[3]); // can access element
+
+// console.log(username.__proto__);   // synatx for prototype(.__proto__)
+
+// console.log(username.toUpperCase());  //  SHUBHMAN
+
+// // Don't forget to use () bcoz this all are functions
+
+// console.log(username.toUpperCase);    // [Function: toUpperCase]
+
+// console.log(username.toLowerCase);  // [Function: toLowerCase] 
+
+// console.log(username.indexOf('u'));
+
+// const newStrDeclare = String ('aakashi');
+// console.log(newStrDeclare.substring(4,8));  //.shi   if we dont gove 4 then it will take 0 as default and it ignore negative values
+
+// // console.log(strDeclare.slice(-8,4)) // it can taek -ve value 
+
