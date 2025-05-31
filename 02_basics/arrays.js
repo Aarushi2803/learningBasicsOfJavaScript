968640b99018a92789e6356a3c9184be0a1e67f0
Aarushi <aarushimandloi744@gmail.com>
2025-06-01 00:31:39 +0530

    array-2

--- 02_basics/arrays.js
+++ 02_basics/arrays.js
@@ -43,12 +43,47 @@
 // slice , splice
 // So, the key difference is that slice() creates a new array without altering the original, while splice() directly changes the original array.  
 
-const a1=[38,92,"smile","be consistent",'hard-work'];
+// const a1=[38,92,"smile","be consistent",'hard-work'];
 
-console.log("silce",a1);
-console.log(a1.slice(2,4));  // it will print from 2 to 3
-console.log("slice willnot modifies the ur current array",a1);  
+// console.log("silce",a1);
+// console.log(a1.slice(2,4));  // it will print from 2 to 3
+// console.log("slice willnot modifies the ur current array",a1);  
 
-console.log("splice",a1);
-console.log(a1.splice(2,4));  // it will print from 2 to 4
-console.log("splice will change the original array",a1);
+// console.log("splice",a1);
+// console.log(a1.splice(2,4));  // it will print from 2 to 4
+// console.log("splice will change the original array",a1);
+
+
+//ARRAYS -2 
+
+const child = ['aaru','aakashi','love']
+const parent = ['raksha','mukesh']
+
+// if you write  child.push(parent) inside the console it will give you number in this case answer will be 4 
+
+// child.push(parent)
+// console.log('push() ',child); 
+ 
+// console.log('concat() ',child.concat(parent)); 
+ 
+// const family_members = [...child , ...parent]
+// console.log(family_members);
+
+
+const using = [1,2,3,[4,5,6],[24,[3,5,6]]];
+console.log(using.flat()); // [ 1, 2, 3, 4, 5, 6, 24, [ 3, 5, 6 ] ]
+console.log(using.flat(2));
+// [
+//  1,  2, 3, 4, 5,
+// 6, 24, 3, 5, 6
+// ]
+ 
+console.log(using.flat(0)); //[ 1, 2, 3, [ 4, 5, 6 ], [ 24, [ 3, 5, 6 ] ] ]
+
+// check wjeather it is array or not 
+
+console.log(Array.isArray("Aarshi"));
+console.log(Array.from("Aarushi"));  // FROM will cnvert it into string
+
+console.log(Array.of({}));
+
--- 02_basics/tempCodeRunnerFile.js
+++ 02_basics/tempCodeRunnerFile.js
@@ -1,2 +1,1 @@
-const arr = [0,1,2,3.4,true,"aarushi"];
-// console.log(arr);   
+child
