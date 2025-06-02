d905f1cd1742a699ae43cefba1a8765ad287243e
Aarushi <aarushimandloi744@gmail.com>
2025-06-02 09:21:16 +0530

    scope-1

---
+++ 03_basics/scope.js
@@ -0,0 +1,20 @@
+// let a = 10;  // we cannot use same variable again and again in let
+// let a =10; // error 
+
+// var c = 20;  /// we can use same variable again and again in var 
+// var c =902;
+
+// const b = 30; // we cannot use same variable again and again in let
+// const b = 34;
+// console.log(a,b,c);
+ 
+let a =20,b=4,c=43;
+var  d=20,f=4,g=43;
+const e=12,q=21,w=2;
+
+if(true){      
+   console.log(a+b+c);
+   console.log(d+f+g);
+   console.log(q+e+w); 
+   // can access values inside a block 
+}
