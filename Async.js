e7f4b28c89f19560c928b1f8d3adf3821ff55a50
Aarushi <aarushimandloi744@gmail.com>
2025-06-16 22:49:05 +0530

    Async

---
+++ Async.js
@@ -0,0 +1,17 @@
+// DEFAULT => js is synchronous and single threaded  
+
+// # execution context
+// 1. one line of code at a time 
+// 2. each opreation waits for the lats one tom complete before ececuting.
+
+// blocking code   
+// 1. block the flow of program
+// 2. read fie syncb
+
+
+// NON-blocking code   
+// 1. Does not block the flow of program
+// 2. read fie Async
+
+
+// js engine =>  1.meomory heap 2.call stack
